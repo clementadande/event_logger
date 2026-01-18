87ce6ed7cdf6ada1966069456829762d3038483e
clementadande <clement.adande@alouette.ai>
2026-01-18 04:05:31 +0100

    fix: specify the right dependance

--- event_logger/insights.py
+++ event_logger/insights.py
@@ -8,7 +8,7 @@ from typing import List, Dict, Optional, Tuple
 from itertools import groupby
 import warnings
 warnings.filterwarnings('ignore')
-from processor import EventProcessor
+from .processor import EventProcessor
 
 class InsightExtractor:
     """
